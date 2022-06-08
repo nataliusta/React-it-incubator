1b1c2451a41c77752f17105da00a6f21f3697c7a
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-06-08 18:15:57 +0300

    add grid-content

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp88JRLfvIPwabmfdyD-Vh11ptnX0TnPBSng&usqp=CAU' alt='chanel logo' />
       </header>
+      <nav className='navigation'>
+        <ul>
+          <li>Profile</li>
+          <li>Messages</li>
+          <li>News</li>
+          <li>Music</li>
+          <li>Settings</li>
+        </ul>
+      </nav>
+      <div className='main-content'>
+        Main content
+      </div>
     </div>
   );
 }
