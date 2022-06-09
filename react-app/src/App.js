476a2fcf28699e0af8a8d26dbde44f0e1a0dd488
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-06-09 16:16:10 +0300

    add footer and stylisation

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
 import MainPage from './components/MainPage';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Navigation />
       <MainPage />
+      <Footer />
     </div>
   );
 }
---
+++ react-app/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+    return (
+       <footer classname='footer'>
+           <div>
+               <ul>
+                   <li>Logo</li>
+                   <li>Phone</li>
+                   <li>Copyright</li>
+               </ul>
+           </div>
+       </footer> 
+    )
+}
+
+export default Footer;
