624868645f6f6bb4127128e990fb31c24502eb8d
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-06-19 22:08:43 +0300

    create new components folders

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Navigation from './components/Navigation';
-import MainPage from './components/MainPage';
-import Footer from './components/Footer';
+import Header from './components/Header/Header';
+import Navigation from './components/Navigation/Navigation';
+import MainPage from './components/MainPage/MainPage';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
