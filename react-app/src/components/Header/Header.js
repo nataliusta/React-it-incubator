50bcb7d8c368e8d27e78021b6299dcd6260d2a32
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-06-22 01:51:40 +0300

    add changes in Header.js and Navigation.js

--- react-app/src/components/Header/Header.js
+++ react-app/src/components/Header/Header.js
@@ -4,7 +4,8 @@ import classes from './Header.module.css';
 function Header() {
     return (
         <header className={classes.header}>
-          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp88JRLfvIPwabmfdyD-Vh11ptnX0TnPBSng&usqp=CAU' alt='Chanel logo' />
+          <img src='https://i.pinimg.com/736x/7a/6f/7e/7a6f7e9c3d564feb516bff7e62e3cc33.jpg' alt='Chanel logo' />
+          <img src='https://cdn.shopify.com/s/files/1/1078/7736/products/FANCLUBFINALLOGO.png?v=1585045856' alt='Fun club logo' />
         </header>
     )
 }
--- react-app/src/components/Navigation/Navigation.js
+++ react-app/src/components/Navigation/Navigation.js
@@ -5,10 +5,10 @@ function Navigation() {
     return (
         <nav className={classes.navigation}>
           <ul className={classes.menu}>
-            <li className={`${classes.item} ${classes.active}`}>About us</li>
-            <li className={classes.item}>Catalog</li>
+            <li className={`${classes.item} ${classes.active}`}>Home</li>
+            <li className={classes.item}>Forum</li>
             <li className={classes.item}>News</li>
-            <li className={classes.item}>Contact us</li>
+            <li className={classes.item}>Contacts</li>
             <li className={classes.item}>Gallery</li>
         </ul>
       </nav>
