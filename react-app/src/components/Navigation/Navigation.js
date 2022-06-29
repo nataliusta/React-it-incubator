0c85be7f84f6b29e60d14d1f1a9a567d8e5d9737
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-06-29 15:48:00 +0300

    updated NavLink in Navigation.js

--- react-app/src/components/Navigation/Navigation.js
+++ react-app/src/components/Navigation/Navigation.js
@@ -2,24 +2,26 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import classes from './Navigation.module.css';
 
+const activeLink = ({isActive}) => isActive ? classes.active : classes.item; 
+
 function Navigation() {
     return (
         <nav className={classes.navigation}>
           <ul className={classes.menu}>
             <li className={`${classes.item} ${classes.active}`}>
-              <NavLink to='/mainpage'>Home</NavLink>
+              <NavLink to='/mainpage' className={activeLink}>Home</NavLink>
             </li>
             <li className={classes.item}>
-              <NavLink to='/forum'>Forum</NavLink>
+              <NavLink to='/forum' className={activeLink}>Forum</NavLink>
             </li>
             <li className={classes.item}>
-              <NavLink to='/news'>News</NavLink>
+              <NavLink to='/news' className={activeLink}>News</NavLink>
             </li>
             <li className={classes.item}>
-              <NavLink to='/contacts'>Contacts</NavLink>
+              <NavLink to='/contacts' className={activeLink}>Contacts</NavLink>
             </li>
             <li className={classes.item}>
-              <NavLink to='/gallery'>Gallery</NavLink>
+              <NavLink to='/gallery' className={activeLink}>Gallery</NavLink>
             </li>
         </ul>
       </nav>
