423c44c623c32394586f3d8d3c3b0564f17d251e
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-07-06 16:43:55 +0300

    add props in Forum.js

--- react-app/src/components/Forum/Forum.js
+++ react-app/src/components/Forum/Forum.js
@@ -2,44 +2,42 @@ import React from 'react';
 import classes from './Forum.module.css';
 import { NavLink } from 'react-router-dom';
 
+const DialogItem = (props) => {
+    return (
+        <li className={classes.item + ' ' + classes.active}>
+            <NavLink to={'/forum/' + props.id}>{props.name}</NavLink>
+        </li>
+    )
+}
+
+const MessageItem = (props) => {
+    return (
+        <li className={classes.item}>{props.message}</li>
+    )
+}
+
 const Forum = (props) => {
     return (
         <div className={classes.forumDialogs}>
             <div className={classes.dialogs}>
                 <ul className={classes.dialogsList}>
-                    <li className={classes.item + ' ' + classes.active}>
-                        <NavLink to='/forum/samanta'>Samanta</NavLink>
-                    </li>
-                    <li className={classes.item}>
-                        <NavLink to='/forum/alexa'>Alexa</NavLink>
-                    </li>
-                    <li className={classes.item}>
-                        <NavLink to='/forum/olivia'>Olivia</NavLink>
-                    </li>
-                    <li className={classes.item}>
-                        <NavLink to='/forum/chloe'>Chloe</NavLink>
-                    </li>
-                    <li className={classes.item}>
-                        <NavLink to='/forum/emily'>Emily</NavLink>
-                    </li>
-                    <li className={classes.item}>
-                        <NavLink to='/forum/carla'>Carla</NavLink>
-                    </li>
-                    <li className={classes.item}>
-                        <NavLink to='/forum/jess'>Jess</NavLink>
-                    </li>
-                    <li className={classes.item}>
-                        <NavLink to='/forum/sophie'>Sophie</NavLink>
-                    </li>
+                    <DialogItem name='Samanta' id='samanta' />
+                    <DialogItem name='Alexa' id='alexa' />
+                    <DialogItem name='Olivia' id='olivia' />
+                    <DialogItem name='Chloe' id='chloe' />
+                    <DialogItem name='Emily' id='emily' />
+                    <DialogItem name='Carla' id='carla' />
+                    <DialogItem name='Jess' id='jess' />
+                    <DialogItem name='Sophie' id='sophie' />
                 </ul>
             </div>
             <div className={classes.messages}>
                 <ul className={classes.messagesList}>
-                    <li className={classes.item}>I'm definitely in love with my lipstick!!!</li>
-                    <li className={classes.item}>Hello,could you advice me a good perfume?</li>
-                    <li className={classes.item}>What do you think about Chanel products?</li>
-                    <li className={classes.item}>Hello everyone!!!</li>
-                    <li className={classes.item}>Not sure I liked Chanel Coco Mademoiselle....</li>
+                    <MessageItem message='Definitely in love with my lipstick!!!' />
+                    <MessageItem message='Hello,could you advice me a good perfume?' />
+                    <MessageItem message='What do you think about Chanel products?' />
+                    <MessageItem message='Hello everyone!!!' />
+                    <MessageItem message='Not sure I liked Chanel Coco Mademoiselle....' />
                 </ul>
             </div>
             <div></div>
