5643eb87391a2b0976e7a65f6d9df2195863302a
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-07-18 00:25:21 +0300

    pass data through state

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,31 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-let dialogsData = [
-  {id: 'samanta', name: 'Samanta'},
-  {id: 'alexa', name: 'Alexa'},
-  {id: 'olivia', name: 'Olivia'},
-  {id: 'chloe', name: 'Chloe'},
-  {id: 'emily', name: 'Emily'},
-  {id: 'carla', name: 'Carla'},
-  {id: 'jess', name: 'Jess'},
-  {id: 'sophie', name: 'Sophie'}
-]
-
-let messagesData = [
-  {id: 1, message: 'Definitely in love with my lipstick!!!'},
-  {id: 2, message: 'Hello,could you advice me a good perfume?'},
-  {id: 3, message: 'What do you think about Chanel products?'},
-  {id: 4, message: 'Hello everyone!!!'},
-  {id: 5, message: 'Not sure I liked Chanel Coco Mademoiselle....'}
-]
+import state from './redux/state.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App dialogsData={dialogsData} messagesData={messagesData} />
+    <App state={state} />
   </React.StrictMode>
 );
 
