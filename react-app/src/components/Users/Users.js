743ed6d6b2c7bea1111a9226385ea45e82bab519
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-09-04 00:32:07 +0300

    Update Users.js

--- react-app/src/components/Users/Users.js
+++ react-app/src/components/Users/Users.js
@@ -4,16 +4,19 @@ import * as axios from 'axios';
 import userPhoto from '../../assets/img/user.png';
 
 const Users = (props) => {
+    let getUsers = () => 
+    {
+        if (props.users.length === 0) {
 
-    if (props.users.length === 0) {
-
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items);
-        });
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                props.setUsers(response.data.items);
+            });
+        }
     }
 
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
            {
             props.users.map( u => <div key={u.id}>
                 <div>
