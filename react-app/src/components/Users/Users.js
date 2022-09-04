45398b412d8ddf6615902aed910a4eb5d01e34e7
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-09-04 19:31:42 +0300

    Add constructor in class Users

--- react-app/src/components/Users/Users.js
+++ react-app/src/components/Users/Users.js
@@ -5,20 +5,16 @@ import userPhoto from '../../assets/img/user.png';
 
 class Users extends React.Component {
 
-    getUsers = () => // a method of the class component
-    {
-        if (this.props.users.length === 0) {
-
+    constructor(props) {
+        super(props);
             axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
                 this.props.setUsers(response.data.items);
             });
-        }
     }
 
     render() {
         return (
             <div>
-                <button onClick={this.getUsers}>Get Users</button>
                {
                 this.props.users.map( u => <div key={u.id}>
                     <div>
