e948a7266bf87ecd7fa1391b9989484d6b534c3d
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-09-04 20:49:57 +0300

    Add componentDidMount

--- react-app/src/components/Users/Users.js
+++ react-app/src/components/Users/Users.js
@@ -4,10 +4,9 @@ import * as axios from 'axios';
 import userPhoto from '../../assets/img/user.png';
 
 class Users extends React.Component {
-
-    constructor(props) {
-        super(props);
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
                 this.props.setUsers(response.data.items);
             });
     }
