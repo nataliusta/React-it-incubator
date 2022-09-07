927372fffba184ab7c653514fa8cc95d6fa675d2
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-09-07 22:40:04 +0300

    Update UsersAPIComponent and Users

---
+++ react-app/src/components/Users/Users.js
@@ -0,0 +1,48 @@
+import React from "react";
+import classes from './Users.module.css';
+import userPhoto from '../../assets/img/user.png';
+
+let Users = (props) => {
+
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+        let pages = [];
+        for (let i=1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+
+    return <div>
+        <div>
+            {pages.map(p => {
+                return <span className={props.currentPage === p && classes.selectedPage} 
+                onClick={(e) => {props.onPageChanged(p)}}>{p}</span>
+            })}
+        </div>
+        {
+        props.users.map( u => <div key={u.id}>
+            <div>
+                {u.name}
+            </div>
+            <div>
+                {u.status}
+            </div>
+            <div>
+                {'u.location.country'}
+            </div>
+            <div>
+                {'u.location.city'}
+            </div>
+            <div>
+                <img src={ u.photos.small != null ? u.photos.small : userPhoto } className={classes.userPhoto} />
+            </div>
+            <div>
+                { u.followed 
+                ? <button onClick={ () => { props.unfollow(u.id) } }>Unfollow</button> 
+                : <button onClick={ () => { props.follow(u.id) } }>Follow</button>}
+            </div>
+        </div> )
+        }
+    </div>
+}
+
+export default Users;
--- react-app/src/components/Users/UsersAPIComponent.js.js
+++ react-app/src/components/Users/UsersAPIComponent.js.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import classes from './Users.module.css';
 import * as axios from 'axios';
-import userPhoto from '../../assets/img/user.png';
+import Users from './Users';
 
 class UsersAPIComponent extends React.Component {
     componentDidMount() {
@@ -21,50 +20,13 @@ class UsersAPIComponent extends React.Component {
     }
 
     render() {
-
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-
-        let pages = [];
-        for (let i=1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-
-        return (
-            <div>
-                <div>
-                    {pages.map(p => {
-                        return <span className={this.props.currentPage === p && classes.selectedPage} 
-                        onClick={(e) => {this.onPageChanged(p)}}>{p}</span>
-                    })}
-
-                </div>
-               {
-                this.props.users.map( u => <div key={u.id}>
-                    <div>
-                        {u.name}
-                    </div>
-                    <div>
-                        {u.status}
-                    </div>
-                    <div>
-                        {'u.location.country'}
-                    </div>
-                    <div>
-                        {'u.location.city'}
-                    </div>
-                    <div>
-                        <img src={ u.photos.small != null ? u.photos.small : userPhoto } className={classes.userPhoto} />
-                    </div>
-                    <div>
-                        { u.followed 
-                        ? <button onClick={ () => { this.props.unfollow(u.id) } }>Unfollow</button> 
-                        : <button onClick={ () => { this.props.follow(u.id) } }>Follow</button>}
-                    </div>
-                </div> )
-               }
-            </div>
-        )
-
+        return <Users totalUsersCount={this.props.totalUsersCount} 
+                      pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      onPageChanged={this.onPageChanged}
+                      users={this.props.users}
+                      follow={this.props.follow}
+                      unfollow={this.props.unfollow} /> 
     }
 }
 
