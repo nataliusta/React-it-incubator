2bd0f535bad91a099dbcaa25d2309d5c0732ba76
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-09-07 22:57:26 +0300

    Update UsersContainer.js

--- react-app/src/components/Users/UsersContainer.js
+++ react-app/src/components/Users/UsersContainer.js
@@ -4,7 +4,7 @@ import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountA
 import * as axios from 'axios';
 import Users from './Users';
 
-class UsersAPIComponent extends React.Component {
+class UsersContainer extends React.Component {
     componentDidMount() {
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
             .then(response => {
@@ -62,5 +62,4 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-const UsersContainer = connect (mapStateToProps, mapDispatchToProps) (UsersAPIComponent);
-export default UsersContainer;
+export default connect (mapStateToProps, mapDispatchToProps) (UsersContainer);
