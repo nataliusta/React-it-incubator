79c0dd350ac1000088d51112892c105a32519063
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-09-08 00:26:55 +0300

    Create isFetching

--- react-app/src/components/Users/UsersContainer.js
+++ react-app/src/components/Users/UsersContainer.js
@@ -22,13 +22,16 @@ class UsersContainer extends React.Component {
     }
 
     render() {
-        return <Users totalUsersCount={this.props.totalUsersCount} 
-                      pageSize={this.props.pageSize}
-                      currentPage={this.props.currentPage}
-                      onPageChanged={this.onPageChanged}
-                      users={this.props.users}
-                      follow={this.props.follow}
-                      unfollow={this.props.unfollow} /> 
+        return <>
+            <Users totalUsersCount={this.props.totalUsersCount} 
+                        pageSize={this.props.pageSize}
+                        currentPage={this.props.currentPage}
+                        onPageChanged={this.onPageChanged}
+                        users={this.props.users}
+                        follow={this.props.follow}
+                        unfollow={this.props.unfollow} 
+            /> 
+        </>
     }
 }
 
@@ -37,7 +40,8 @@ let mapStateToProps = (state) => {
         users: state.usersPage.users,
         pageSize: state.usersPage.pageSize,
         totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage
+        currentPage: state.usersPage.currentPage,
+        isFetching: state.userPage.isFetching
     }
 }
 
