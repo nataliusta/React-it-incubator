fec8e029bccff76bd9fe0ac5ba826cb3030090be
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-09-12 01:30:21 +0300

    Add useParams

--- react-app/src/components/MainPage/MainPageContainer.js
+++ react-app/src/components/MainPage/MainPageContainer.js
@@ -3,10 +3,13 @@ import {connect} from 'react-redux';
 import MainPage from './MainPage';
 import * as axios from 'axios';
 import { setUserProfile } from '../../redux/main-reducer';
+import { useParams } from 'react-router-dom';
+
 
 class MainPageContainer extends React.Component {
     
     componentDidMount() {
+        debugger;
         axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
             .then(response => {
                 this.props.setUserProfile(response.data);
@@ -26,5 +29,8 @@ let mapStateToProps = (state) => ({
     profile: state.mainPage.profile
 });
 
+const TakeParams = (props) => {
+    return <MainPageContainer {...props} param={useParams()} match={match} />
+}
 
-export default connect(mapStateToProps, {setUserProfile}) (MainPageContainer);
+export default connect(mapStateToProps, {setUserProfile}) (TakeParams);
