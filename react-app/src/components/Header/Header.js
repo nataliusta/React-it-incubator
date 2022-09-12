1dbc49c08a8076d210d58a4577f4d1b0dbfe99be
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-09-13 00:46:39 +0300

    Create Login in Header

--- react-app/src/components/Header/Header.js
+++ react-app/src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './Header.module.css';
 
 function Header() {
@@ -7,6 +8,7 @@ function Header() {
           <img src='https://i.pinimg.com/736x/7a/6f/7e/7a6f7e9c3d564feb516bff7e62e3cc33.jpg' alt='Chanel logo' />
           <img src='https://cdn.shopify.com/s/files/1/1078/7736/products/FANCLUBFINALLOGO.png?v=1585045856' alt='Fun club logo' />
           <div className={classes.loginBlock}>
+            <NavLink to={'/login'}>Login</NavLink>
           </div>
         </header>
     )
