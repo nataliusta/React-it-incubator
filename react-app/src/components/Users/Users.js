ca83369e34f24c0e747e76f39ce455de890c06ed
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-09-13 18:08:23 +0300

    Create POST and DELETE requests for Follow/Unfollow

--- react-app/src/components/Users/Users.js
+++ react-app/src/components/Users/Users.js
@@ -2,6 +2,7 @@ import React from "react";
 import classes from './Users.module.css';
 import userPhoto from '../../assets/img/user.png';
 import { NavLink } from "react-router-dom";
+import * as axios from 'axios';
 
 let Users = (props) => {
 
@@ -40,8 +41,36 @@ let Users = (props) => {
             </div>
             <div>
                 { u.followed 
-                ? <button onClick={ () => { props.unfollow(u.id) } }>Unfollow</button> 
-                : <button onClick={ () => { props.follow(u.id) } }>Follow</button>}
+                ? <button onClick={ () => { 
+
+                    axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                        withCredentials: true,
+                        headers: {
+                            'API-KEY': '23f0d013-825b-4922-8b88-0221bfa9c74a'
+                        }
+                    })
+                        .then(response => {
+                            if (response.data.resultCode == 0) {
+                                props.unfollow(u.id); 
+                            }
+                        });
+
+                }}>Unfollow</button> 
+                : <button onClick={ () => { 
+
+                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                        withCredentials: true,
+                        headers: {
+                            'API-KEY': '23f0d013-825b-4922-8b88-0221bfa9c74a'
+                        }
+                    })
+                        .then(response => {
+                            if (response.data.resultCode == 0) {
+                                props.follow(u.id);
+                            }
+                        });
+
+                }}>Follow</button>}
             </div>
         </div> )
         }
--- react-app/src/components/Users/UsersContainer.js
+++ react-app/src/components/Users/UsersContainer.js
@@ -8,7 +8,9 @@ import Preloader from '../Preloader/Preloader.js';
 class UsersContainer extends React.Component {
     componentDidMount() {
         this.props.toggleIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
+            withCredentials: true
+        })
             .then(response => {
                 this.props.toggleIsFetching(false);
                 this.props.setUsers(response.data.items);
@@ -19,7 +21,9 @@ class UsersContainer extends React.Component {
     onPageChanged = (pageNumber) => {
         this.props.toggleIsFetching(true);
         this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {
+            withCredentials: true
+        })
             .then(response => {
                 this.props.toggleIsFetching(false);
                 this.props.setUsers(response.data.items);
