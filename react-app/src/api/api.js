7dd27b16af25f5e8f4903a79f2760ba3d9e14600
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-09-14 16:42:36 +0300

    Create axios.create

--- react-app/src/api/api.js
+++ react-app/src/api/api.js
@@ -1,5 +1,28 @@
 import * as axios from 'axios';
 
-axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
-            withCredentials: true
+const baseUrl = 'https://social-network.samuraijs.com/api/1.0/';
+
+const instance = axios.create({
+    withCredentials: true,
+    headers: {
+        'API-KEY': '23f0d013-825b-4922-8b88-0221bfa9c74a'
+    }
+});
+
+export const getUsers = (currentPage = 1, pageSize = 10) => {
+    return axios.get(baseUrl + `users?page=${currentPage}&count=${pageSize}`, {
+                withCredentials: true
         })
+        .then(response => {
+            return response.data;
+    }); // promise where there is only data
+};
+
+/*export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+    return axios.get(baseUrl + `users?page=${currentPage}&count=${pageSize}`, {
+                withCredentials: true
+        })
+        .then(response => {
+            return response.data;
+    });
+};*/
