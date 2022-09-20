8829fdc77187ae69a46896c22bd6336ab262694f
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-09-20 18:24:37 +0300

    Update profileAPI

--- react-app/src/api/api.js
+++ react-app/src/api/api.js
@@ -23,13 +23,20 @@ export const usersAPI = {
         
     },
     getProfile(userId) {
-        return instance.get(`profile/` + userId);
+        console.warn('Obsolete method. Please profileAPI object.');
+        return profileAPI.getProfile(userId);
     }
 }
 
 export const profileAPI = {
     getProfile(userId) {
         return instance.get(`profile/` + userId);
+    },
+    getStatus(userId) {
+        return instance.get(`status/` + userId);
+    },
+    updateStatus(status) {
+        return instance.put(`status`);
     }
 }
 
