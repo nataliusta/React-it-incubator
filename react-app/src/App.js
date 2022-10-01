363e1be9bc68f30342de6dbf07c429999ed32d7e
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-10-01 20:24:01 +0300

    Create HOC function for ComponentWithRouterProp in App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -10,28 +10,57 @@ import News from './components/News/News';
 import Contacts from './components/Contacts/Contacts';
 import UsersContainer from './components/Users/UsersContainer';
 import Login from './components/Login/Login';
+import { getAuthUserData } from './redux/auth-reducer';
+import {connect} from 'react-redux';
+import { compose } from 'redux';
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <div className='app-wrapper'>
-        <HeaderContainer />
-        <Navigation />
-        <div className='app-wrapper-content'>
-          <Routes>
-            <Route path='/mainpage/:userId' element={<MainPageContainer />} />
-            <Route path='/mainpage/*' element={<MainPageContainer />} />
-            <Route path='/forum/*' element={<ForumContainer />} />
-            <Route path='/news' element={<News />} />
-            <Route path='/contacts' element={<Contacts />} />
-            <Route path='/users' element={<UsersContainer />} />
-            <Route path='/login' element={<Login />} />
-          </Routes>
+class App extends React.Component {
+
+  componentDidMount() {
+    this.props.getAuthUserData();
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <div className='app-wrapper'>
+          <HeaderContainer />
+          <Navigation />
+          <div className='app-wrapper-content'>
+            <Routes>
+              <Route path='/mainpage/:userId' element={<MainPageContainer />} />
+              <Route path='/mainpage/*' element={<MainPageContainer />} />
+              <Route path='/forum/*' element={<ForumContainer />} />
+              <Route path='/news' element={<News />} />
+              <Route path='/contacts' element={<Contacts />} />
+              <Route path='/users' element={<UsersContainer />} />
+              <Route path='/login' element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App;
+const withRouter = (Component) => { // HOC
+
+  const ComponentWithRouterProp = (props) => { // Container component
+      let location = useLocation();
+      let navigate = useNavigate();
+      let params = useParams();
+      return (
+          <Component
+              {...props}
+              router={{ location, navigate, params }}
+          />
+      );
+  }
+
+  return ComponentWithRouterProp;
+}
+
+export default compose(
+  withRouter,
+  connect(null, {getAuthUserData}))(App);
