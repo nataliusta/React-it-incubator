57752e3b525f69b9986622c004643a0c26a3cc44
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-10-05 23:51:00 +0300

    Update UsersContainer.js

--- react-app/src/redux/users-reducer.js
+++ react-app/src/redux/users-reducer.js
@@ -87,10 +87,11 @@ export const setTotalUsersCount = (totalUsersCount) => ({type: SET_TOTAL_USERS_C
 export const toggleIsFetching = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching})
 export const toggleFollowingProgress = (isFetching, userId) => ({type: TOGGLE_IS_FOLLOWING_PROGRESS, isFetching, userId})
 
-export const requestUsers = (currentPage, pageSize) => { // a pure thunk function for setUsers that dispatches actions
+export const requestUsers = (page, pageSize) => { // a pure thunk function for setUsers that dispatches actions
     return (dispatch) => {
         dispatch(toggleIsFetching(true));
-        usersAPI.getUsers(currentPage, pageSize)
+        dispatch(setCurrentPage(page));
+        usersAPI.getUsers(page, pageSize)
             .then(data => {
                 dispatch(toggleIsFetching(false));
                 dispatch(setUsers(data.items));
