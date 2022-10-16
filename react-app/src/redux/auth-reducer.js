ebfd9237514eb4f674b8a7391235259a8b42a3b5
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-10-16 16:19:46 +0300

    Add async/await to auth-reducer

--- react-app/src/redux/auth-reducer.js
+++ react-app/src/redux/auth-reducer.js
@@ -1,7 +1,7 @@
 import { stopSubmit } from 'redux-form';
 import {authAPI} from '../api/api';
 
-const SET_USER_DATA = 'SET_USER_DATA';
+const SET_USER_DATA = 'samurai-network/auth/SET_USER_DATA';
 
 
 let initialState = {
@@ -14,12 +14,10 @@ let initialState = {
 const authReducer = (state = initialState, action) => {
     switch(action.type) {
         case SET_USER_DATA:
-            
             return {
                 ...state,
                 ...action.payload
             }
-
         default:
             return state; 
     }
@@ -28,36 +26,32 @@ const authReducer = (state = initialState, action) => {
 export const setAuthUserData = (userId, email, login, isAuth) => ({type: SET_USER_DATA, payload: 
          {userId, email, login, isAuth}}) 
 
-export const getAuthUserData = () => (dispatch) => {
-    authAPI.me()
-        .then(response => {
-            if (response.data.resultCode === 0) {
-                let {id, login, email} = response.data.data;
-                dispatch(setAuthUserData(id, email, login, true));
-            }
-        });
+export const getAuthUserData = () => async (dispatch) => { // async/await
+    let response = await authAPI.me();
+
+    if (response.data.resultCode === 0) {
+        let {id, login, email} = response.data.data;
+        dispatch(setAuthUserData(id, email, login, true));
+    }
 }
 
-export const login = (email, password, rememberMe) => (dispatch) => {
+export const login = (email, password, rememberMe) => async  (dispatch) => { // async/await
 
-    authAPI.login(email, password, rememberMe)
-        .then(response => {
-            if (response.data.resultCode === 0) {
-                dispatch(getAuthUserData());
-            } else {
-                let message = response.data.messages.length > 0 ? response.data.messages[0] : 'Some error';
-                dispatch(stopSubmit('login', {_error: message})); 
-            }
-        });
+    let response = await authAPI.login(email, password, rememberMe);
+
+    if (response.data.resultCode === 0) {
+        dispatch(getAuthUserData());
+    } else {
+        let message = response.data.messages.length > 0 ? response.data.messages[0] : 'Some error';
+        dispatch(stopSubmit('login', {_error: message})); 
+    }
 }
 
-export const logout = () => (dispatch) => {
-    authAPI.logout()
-        .then(response => {
-            if (response.data.resultCode === 0) {
-                dispatch(setAuthUserData(null, null, null, false));
-            }
-        });
+export const logout = () =>  async (dispatch) => { // async/await
+    let response = await authAPI.logout();
+    if (response.data.resultCode === 0) {
+        dispatch(setAuthUserData(null, null, null, false));
+    }
 }
 
 export default authReducer;
