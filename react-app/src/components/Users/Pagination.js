4bc6cb076a93d62c3a6466d5d0abcd17cf6095f7
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-10-19 16:57:36 +0300

    Update Pagination.js

--- react-app/src/components/Users/Pagination.js
+++ react-app/src/components/Users/Pagination.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useState} from 'react';
 import classes from './Pagination.module.css';
+import cn from 'classnames';
 
-let Pagination = ({totalUsersCount, pageSize, currentPage, onPageChanged}) => {
+let Pagination = ({totalUsersCount, pageSize, currentPage, onPageChanged, portionSize = 10}) => {
 
     let pagesCount = Math.ceil(totalUsersCount / pageSize);
 
@@ -10,11 +11,27 @@ let Pagination = ({totalUsersCount, pageSize, currentPage, onPageChanged}) => {
             pages.push(i);
         }
 
-    return <div>
-            {pages.map(p => {
-                return <span className={currentPage === p && classes.selectedPage} 
-                onClick={(e) => {onPageChanged(p)}}>{p}</span>
-            })}
+        let portionCount = Math.ceil(pagesCount / portionSize);
+        let [portionNumber, setPortionNumber] = useState(1);
+        let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+        let rightPortionPageNumber = portionNumber * portionSize;
+
+    return <div className={classes.pagination}>
+        {portionNumber > 1 && 
+        <button onClick={() => {setPortionNumber(portionNumber - 1)}}>PREV</button>}
+
+            {pages.filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+                .map((p) => {
+                    return <span className={cn({
+                        [classes.selectedPage] : currentPage === p},
+                         classes.pageNumber) }
+                         key = {p}
+                         onClick={(e) => {
+                            onPageChanged(p);
+                        }}>{p}</span>
+                })}
+            {portionCount > portionNumber && 
+            <button onClick={() => {setPortionNumber(portionNumber + 1)}}>NEXT</button> }
         </div>
 }
 
