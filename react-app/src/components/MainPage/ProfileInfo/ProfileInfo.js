6d772e5c668a272df59f9969759e60ca052ffde9
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2022-10-29 00:52:01 +0300

    Update ProfileInfo.js

--- react-app/src/components/MainPage/ProfileInfo/ProfileInfo.js
+++ react-app/src/components/MainPage/ProfileInfo/ProfileInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 import Preloader from "../../Preloader/Preloader";
 import classes from './ProfileInfo.module.css';
 import ProfileStatusWithHooks from './ProfileStatusWithHooks';
+import userPhoto from '../../../assets/img/user.png';
 
 const ProfileInfo = ({profile, status, updateStatus}) => {
 
@@ -12,7 +13,7 @@ const ProfileInfo = ({profile, status, updateStatus}) => {
     return (
         <div className={classes.profile}>
             <div className={classes.descriptionBlock}>
-                <img src={profile.photos.large} className={classes.avatar} alt='Avatar' />
+                <img src={profile.photos.large || userPhoto} className={classes.avatar} alt='Avatar' />
                 <ProfileStatusWithHooks status={status} updateStatus={updateStatus} />
             </div>
         </div>
